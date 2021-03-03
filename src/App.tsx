d167184793c6283e9ae0219836176fba53cfe5fa
Juliano Ramos <490meister@gmail.com>
2021-03-03 10:12:40 -0300

    meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <HelmetProvider>
-    <Router>    
-      <Routes />        
-    </Router>
-  </HelmetProvider>
+  <Router>    
+    <Routes />        
+  </Router>
 );
 
 export default App;
