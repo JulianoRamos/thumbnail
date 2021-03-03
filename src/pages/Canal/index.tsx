f957b9d5baef78d20c91609b7ffbf0d6873caa14
Juliano Ramos <490meister@gmail.com>
2021-03-03 10:01:37 -0300

    meta tags

--- src/pages/Canal/index.tsx
+++ src/pages/Canal/index.tsx
@@ -6,8 +6,14 @@ const Canal: React.FC = () => {
     <>
       <h1>Canal</h1>
       <Helmet>
-        <title>Hello World</title>
-        <link rel="canonical" href="https://www.tacobell.com/" />
+        <title>Canal</title>
+        <meta property="og:site_name" content="Canal" />
+        <meta property="og:url" content="https://www.youtube.com/watch?v=VUwU9wt2s7w" />
+        <meta property="og:title" content="TITULO NOVO PARA EXEMPLO" />
+        <meta property="og:image" content="https://i.ytimg.com/vi/VUwU9wt2s7w/maxresdefault.jpg" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="720" />
+        <meta property="og:description" content="Description do canal que vai longeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee ..." />
       </Helmet>
     </>
   );
